0c83cee5d670eec633a28a56dd31db1b7deb62c7
yong <zengjunyong@gmail.com>
2018-07-11 20:45:17 +0800

    Dynamic Columns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,19 @@ import {CarService} from './CarService';
 })
 export class AppComponent implements OnInit {
   cars: Car[];
+  cols: any[];
 
   constructor(private carService: CarService) {
   }
 
   ngOnInit() {
     this.carService.getCarsSmall().then(cars => this.cars = cars);
+
+    this.cols = [
+      { field: 'vin', header: 'Vin' },
+      { field: 'year', header: 'Year' },
+      { field: 'brand', header: 'Brand' },
+      { field: 'color', header: 'Color' }
+    ];
   }
 }

13784a012c05d0be9a0fea77f107b9ce5a0dfcf9
yong <zengjunyong@gmail.com>
2018-07-11 21:41:20 +0800

    Sorting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Car} from './Car';
 import {CarService} from './CarService';
+import {SortEvent} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -8,25 +9,50 @@ import {CarService} from './CarService';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  cars: Car[];
-  cols: any[];
-  first = 0;
+  cars1: Car[];
 
-  constructor(private carService: CarService) { }
+  cars2: Car[];
+
+  cars3: Car[];
+
+  cols: any[];
+
+  constructor(private carService: CarService) {
+  }
 
   ngOnInit() {
-    this.carService.getCarsSmall().then(cars => this.cars = cars);
+    this.carService.getCarsSmall().then(cars => this.cars1 = cars);
+    this.carService.getCarsSmall().then(cars => this.cars2 = cars);
+    this.carService.getCarsSmall().then(cars => this.cars3 = cars);
 
     this.cols = [
-      { field: 'vin', header: 'Vin' },
-      { field: 'year', header: 'Year' },
-      { field: 'brand', header: 'Brand' },
-      { field: 'color', header: 'Color' }
+      {field: 'vin', header: 'Vin'},
+      {field: 'year', header: 'Year'},
+      {field: 'brand', header: 'Brand'},
+      {field: 'color', header: 'Color'}
     ];
   }
 
-  reset() {
-    this.first = 0;
+  customSort(event: SortEvent) {
+    event.data.sort((data1, data2) => {
+      const value1 = data1[event.field];
+      const value2 = data2[event.field];
+      let result = null;
+
+      if (value1 == null && value2 != null) {
+        result = -1;
+      } else if (value1 != null && value2 == null) {
+        result = 1;
+      } else if (value1 == null && value2 == null) {
+        result = 0;
+      } else if (typeof value1 === 'string' && typeof value2 === 'string') {
+        result = value1.localeCompare(value2);
+      } else {
+        result = (value1 < value2) ? -1 : (value1 > value2) ? 1 : 0;
+      }
+
+      return (event.order * result);
+    });
   }
 
 }
